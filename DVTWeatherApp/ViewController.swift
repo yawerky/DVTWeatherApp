21562eb549390894dae1676f4cc36ce819e296e0
Yawer Khan <yawar@beatroute.io>
2025-10-26 10:31:06 +0530

    Initial project setup

---
+++ DVTWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DVTWeatherApp
+//
+//  Created by Yawer Khan on 26/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DVTWeatherAppTests/DVTWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DVTWeatherAppTests.swift
+//  DVTWeatherAppTests
+//
+//  Created by Yawer Khan on 26/10/25.
+//
+
+import Testing
+@testable import DVTWeatherApp
+
+struct DVTWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
